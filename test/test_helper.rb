fe4bc5ea9ad2b435aa3f5fbaea4a5badb4a549b3
m11o <velonica2227@icloud.com>
2020-04-18 20:08:50 +0900

    feat: update.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,4 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-require "test-unit"
-
-require "rails"
 require "active_storage_cacheable"
 
-Bundler.require
-
-require "test/unit/rails/test_help"
+require "test/unit"
