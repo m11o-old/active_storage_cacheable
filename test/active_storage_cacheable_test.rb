73a85418d458477fd3f9257451c50db37a3bff22
m11o <velonica2227@icloud.com>
2020-04-18 19:03:24 +0900

    feat: change test parent class.
    テストクラスの継承クラスをTest::Unit::TestCaseに変更

--- test/active_storage_cacheable_test.rb
+++ test/active_storage_cacheable_test.rb
@@ -1,10 +1,6 @@
 require "test_helper"
 
-class ActiveStorageCacheableTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::ActiveStorageCacheable::VERSION
-  end
-
+class ActiveStorageCacheableTest < Test::Unit::TestCase
   def test_it_does_something_useful
     assert_true true
   end
